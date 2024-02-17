bd7a598c0ce57261b08057871e039b992a974111
ThibaultVercoutre <thibault.vercoutre@yahoo.com>
2024-02-17 23:35:19 +0100

    connexion

--- lpst/src/pages/Login.tsx
+++ lpst/src/pages/Login.tsx
@@ -1,13 +1,35 @@
+import { useState, useEffect } from "react"
+
 const Login = () => {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        password: ""
+    });
+
+    const connect = () => {
+        if (formData.name === "truc" && formData.password === "truc") {
+            console.log("ok");
+        }
+    }
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    }
+
     return (
         <div id="login">
             <div className="form">
                 <h1>Login</h1>
                 <label htmlFor="username">Username</label>
-                <input type="text" placeholder=""/>
+                <input type="text" placeholder="" value={formData.name} name="name" onChange={handleInputChange}/>
                 <label htmlFor="username">Password</label>
-                <input type="password" placeholder="" />
-                <button type="button" className="login">Se connecter</button>
+                <input type="password" placeholder="" value={formData.password} name="password" onChange={handleInputChange}/>
+                <button onClick={connect} type="button" className="login">Se connecter</button>
                 <a className="signin" href="/signin">S'inscrire</a>
             </div>
         </div>
